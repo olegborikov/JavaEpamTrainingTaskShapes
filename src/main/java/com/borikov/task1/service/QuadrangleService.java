f667adc2978a695c37403730272897ca0bf9f558
Oleg Borikov <olegborikov999@gmail.com>
2020-08-12 10:29:00 +0300

    refactor QuadrangleArithmeticService

--- src/main/java/com/borikov/task1/service/QuadrangleService.java
+++ src/main/java/com/borikov/task1/service/QuadrangleService.java
@@ -3,32 +3,36 @@ package com.borikov.task1.service;
 import com.borikov.task1.entity.Point;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public abstract class QuadrangleService {
+    private static final int ROUND_NUMBER = 5;
+
     protected double calculateDistanceBetweenPoints(
             Point point1, Point point2) {
         double distanceX = point1.getX() - point2.getX();
         double distanceY = point1.getY() - point2.getY();
-        double distanceBetweenPoints = Math.hypot(distanceX, distanceY);// TODO: 10.08.2020 refactor: round
-        return distanceBetweenPoints;
+        double distanceBetweenPoints = Math.hypot(distanceX, distanceY);
+        return round(distanceBetweenPoints);
     }
 
-    protected double calculateCosine(double side1, double side2, double side3) {// TODO: 10.08.2020 refactor
-        BigDecimal s1 = BigDecimal.valueOf(side1);
-        BigDecimal s2 = BigDecimal.valueOf(side2);
-        BigDecimal s3 = BigDecimal.valueOf(side3);
-        BigDecimal s12 = s1.multiply(s1);
-        BigDecimal s22 = s2.multiply(s2);
-        BigDecimal s32 = s3.multiply(s3);
-        BigDecimal first = s12.add(s22).subtract(s32).setScale(5, BigDecimal.ROUND_HALF_UP);
-        BigDecimal second = s1.multiply(s1).multiply(new BigDecimal(2));
-        return first.divide(second, BigDecimal.ROUND_HALF_UP).doubleValue();
+    protected double calculateCosine(double side1, double side2, double side3) {
+        double side1Squared = round(Math.pow(side1, 2));
+        double side2Squared = round(Math.pow(side2, 2));
+        double side3Squared = round(Math.pow(side3, 2));
+        double cosine = (side1Squared + side2Squared - side3Squared) / (side1Squared * side2Squared * 2);
+        return round(cosine);
     }
 
-    protected double getSinusFromCosine(double cosine) {// TODO: 10.08.2020 refactor
-        BigDecimal c = BigDecimal.valueOf(cosine);
-        BigDecimal c1 = c.pow(2);
-        BigDecimal sinus = new BigDecimal("1").subtract(c1);
-        return sinus.setScale(5, BigDecimal.ROUND_HALF_UP).doubleValue();
+    protected double getSinusFromCosine(double cosine) {
+        double cosineSquared = round(Math.pow(cosine, 2));
+        double sinus = 1 - cosineSquared;
+        return round(sinus);
+    }
+
+    protected double round(double number) {
+        BigDecimal newNumber = BigDecimal.valueOf(number);
+        BigDecimal roundedNumber = newNumber.setScale(ROUND_NUMBER, RoundingMode.HALF_UP);
+        return roundedNumber.doubleValue();
     }
 }
--- src/main/java/com/borikov/task1/service/impl/QuadrangleArithmeticServiceImpl.java
+++ src/main/java/com/borikov/task1/service/impl/QuadrangleArithmeticServiceImpl.java
@@ -5,23 +5,27 @@ import com.borikov.task1.exception.IncorrectDataException;
 import com.borikov.task1.service.QuadrangleArithmeticService;
 import com.borikov.task1.validator.QuadrangleValidator;
 
-public class QuadrangleArithmeticServiceImpl extends QuadrangleArithmeticService {
+public class QuadrangleArithmeticServiceImpl extends QuadrangleArithmeticService {// TODO: 11.08.2020 refactor 
     @Override
-    public double calculateSquare(Quadrangle quadrangle) throws IncorrectDataException {// TODO: 10.08.2020 refactor
+    public double calculateSquare(Quadrangle quadrangle) throws IncorrectDataException {
         QuadrangleValidator quadrangleValidator = new QuadrangleValidator();
         if (!quadrangleValidator.isQuadrangleCorrect(quadrangle)) {
             throw new IncorrectDataException("Data is incorrect");
         }
-        double quadrangleSquare = 0;
         double side1 = calculateDistanceBetweenPoints(
                 quadrangle.getPoint1(), quadrangle.getPoint2());
         double side2 = calculateDistanceBetweenPoints(
                 quadrangle.getPoint1(), quadrangle.getPoint4());
         double side3 = calculateDistanceBetweenPoints(
                 quadrangle.getPoint2(), quadrangle.getPoint4());
+        double diagonal1 = calculateDistanceBetweenPoints(
+                quadrangle.getPoint1(), quadrangle.getPoint2());
+        double diagonal2 = calculateDistanceBetweenPoints(
+                quadrangle.getPoint1(), quadrangle.getPoint4());
         double cosine = calculateCosine(side1, side2, side3);
         double sinus = getSinusFromCosine(cosine);
-        return sinus;
+        double quadrangleSquare = diagonal1 * diagonal2 * sinus * 0.5;
+        return round(quadrangleSquare);
     }
 
     @Override
@@ -39,6 +43,7 @@ public class QuadrangleArithmeticServiceImpl extends QuadrangleArithmeticService
                 quadrangle.getPoint3(), quadrangle.getPoint4());
         double side4 = calculateDistanceBetweenPoints(
                 quadrangle.getPoint4(), quadrangle.getPoint1());
-        return side1 + side2 + side3 + side4;
+        double quadranglePerimeter = side1 + side2 + side3 + side4;
+        return round(quadranglePerimeter);
     }
 }
